439c4c390999e4cf29605f0010f2c8a68750cfc7
Hawk Song <savokisong@gmail.com>
2019-09-07 23:16:19 -0700

    Initial Commit

---
+++ InstagramFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFireBase
+//
+//  Created by Hawk Song on 9/7/19.
+//  Copyright © 2019 Hawk Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
